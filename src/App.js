001f0f7c4ba8f126180700f1dab7436051c7ca68
Ruben Cardoza <rubencardoza611@gmail.com>
2022-07-27 06:32:20 -0700

    archivos iniciales pagina sencilla con react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Testimonio from './componentes/Testimonio.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio 
+          nombre='Alejandra Castillo'
+          pais='Mexico'
+          imagen='Hoshii'
+          cargo='Ingeniera de Software'
+          empresa='Google'
+          alt='Hoshi'
+          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
+
+        <Testimonio 
+          nombre='Emma Bostian'
+          pais='Suecia'
+          imagen='Emma'
+          cargo='Ingeniera de software'
+          empresa='Spotify'
+          alt='Foto de Emma'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' />
+         
+          <Testimonio 
+          nombre='Swhan Wang'
+          pais='Singapur'
+          imagen='Shawn'
+          cargo='Ingeniero de software'
+          empresa='Amazon'
+          alt='Foto de Shawn'
+          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
+         
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
+);//pruba de sonido de mi teclado en este microfono de mierda
+//probando test de micrfono para captar el audio de mi teclado al escribir  codigo en visual studio code usando react js jaja
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
